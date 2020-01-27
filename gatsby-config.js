e9aa1580a2d4123f90ae8db68a4a15f5249b3930
Maeno Isaac Takuma <maenotakuma0927@gmail.com>
2020-01-27 22:02:09 +0900

    [UPDATE]コンフィグ変更
    サイトのメタデータを変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `webStein;`,
+    description: `備忘録`,
+    author: `@isaacmaenu`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
