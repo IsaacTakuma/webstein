a2568d08e2a4f44348814bfbd50912f8d7614452
Maeno Isaac Takuma <maenotakuma0927@gmail.com>
2020-01-27 23:28:47 +0900

    [update]Imageタグ

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,14 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/contents/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/contents/posts`,
+        name: `posts`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -24,7 +31,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `contents/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
