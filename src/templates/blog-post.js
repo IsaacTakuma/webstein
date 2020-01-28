5d83fc01a58c9b2b7785ec2ecb3b87949230a635
Maeno Isaac Takuma <maenotakuma0927@gmail.com>
2020-01-28 11:12:43 +0900

    [fix]blog-post.js
    画像使わないのでコメントアウト

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 class BlogPostTemplate extends React.Component {
