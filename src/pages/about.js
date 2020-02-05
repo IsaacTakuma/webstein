2892841790ad3c55e1318f4cb4817e7aa9ad946a
Maeno Isaac Takuma <maenotakuma0927@gmail.com>
2020-02-06 01:53:08 +0900

    [update]アバウトページ修正
    内容的にみれるようにした

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,9 +8,17 @@ const AboutPage = () => (
   <Layout>
     <SEO title="about" />
     <div>
-      <h1>This is about page</h1>
-      <p>Welcome to about page</p>
-      <Link to="/">Go back to the home</Link>
+      <h1
+        style={{
+          margin: "0 0 1em 0",
+        }}
+      >
+        This is about page
+      </h1>
+      <p>
+        このサイトは静的サイトの制作方法を学ぶためにGatsbyJSの技術を使用して制作したブログです。
+      </p>
+      <p>今後拡張していく予定。</p>
     </div>
   </Layout>
 )
