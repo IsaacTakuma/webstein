51c034dce70a5672a4a712c9fd066faba59c0ea2
Maeno Isaac Takuma <maenotakuma0927@gmail.com>
2020-02-08 03:59:02 +0900

    [fix]seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -12,6 +12,8 @@ function SEO({ description, lang, meta, title }) {
             title
             description
             author
+            siteUrl
+            image
           }
         }
       }
